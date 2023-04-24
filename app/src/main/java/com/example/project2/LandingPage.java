fcd297a1846867f9eaa0ad8c0a157a7b9f6fcfb1
liam <liamc@me.com>
2023-04-24 01:17:37 -0700

    new branch, trying to switch activities without passing values with getExtra()

--- app/src/main/java/com/example/project2/LandingPage.java
+++ app/src/main/java/com/example/project2/LandingPage.java
@@ -18,14 +18,14 @@ public class LandingPage extends AppCompatActivity {
     Button mButton;
 
     public static final String MESSAGE = "com.example.project2";
-    public static Intent intentFactory(Context packageContext, Boolean admin){
+    /*public static Intent intentFactory(Context packageContext, Boolean admin){
         Intent intent = new Intent (packageContext, LandingPage.class);
         intent.putExtra(MESSAGE, admin);
 
         return intent;
-    }
+    }*/
 
-    Boolean logName = getIntent().getBooleanExtra(MESSAGE, true);
+    //Boolean logName = getIntent().getBooleanExtra(MESSAGE, true);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +36,11 @@ public class LandingPage extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Boolean adminStatus = getIntent().getBooleanExtra(MESSAGE,true);
+        /*Boolean adminStatus = getIntent().getBooleanExtra(MESSAGE,true);
 
         if (adminStatus){
             mButton.setVisibility(View.VISIBLE);
-        }
+        }*/
         
     }
 
