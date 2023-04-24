35d3e1afd87f838d1c9c19e50f3a502614e0772d
liam <liamc@me.com>
2023-04-24 00:58:38 -0700

    testing some admin properties

--- app/src/main/java/com/example/project2/LandingPage.java
+++ app/src/main/java/com/example/project2/LandingPage.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.project2.DB.UserDAO;
+import com.example.project2.databinding.ActivityMainBinding;
 
 public class LandingPage extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    Button mButton;
 
     public static final String MESSAGE = "com.example.project2";
     public static Intent intentFactory(Context packageContext, Boolean admin){
@@ -20,10 +27,18 @@ public class LandingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         Boolean adminStatus = getIntent().getBooleanExtra(MESSAGE,true);
+
+        if (adminStatus){
+            mButton.setVisibility(View.VISIBLE);
+        }
         
     }
 
