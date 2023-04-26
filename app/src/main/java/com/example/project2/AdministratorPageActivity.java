003ef89ae78fe1fd53ae73ef16b207bca911365d
liam <liamc@me.com>
2023-04-26 15:38:23 -0700

    wiring up admin page

--- app/src/main/java/com/example/project2/AdministratorPageActivity.java
+++ app/src/main/java/com/example/project2/AdministratorPageActivity.java
@@ -1,20 +1,54 @@
 package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+
+import com.example.project2.databinding.ActivityAdministratorPageBinding;
 
 public class AdministratorPageActivity extends AppCompatActivity {
 
+    ActivityAdministratorPageBinding mAdminPageBinding;
+
+    Button mFleetViewButton;
+    Button mShipViewButton;
+
     public static final String MESSAGE = "message1";
     public static final String MESSAGE_1 = "message2";
+
+    TableLayout mTableLayout;
+    ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrator_page);
+
+        mAdminPageBinding = ActivityAdministratorPageBinding.inflate(getLayoutInflater());
+        setContentView(mAdminPageBinding.getRoot());
+
+        mFleetViewButton = mAdminPageBinding.fleetViewButton;
+        mShipViewButton = mAdminPageBinding.shipViewButton;
+
+        mFleetViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mShipViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     public static Intent intentFactory(Context packageContext, Boolean isAdmin, String username){
@@ -23,4 +57,5 @@ public class AdministratorPageActivity extends AppCompatActivity {
         intent.putExtra(MESSAGE_1, username);
         return intent;
     }
+
 }
